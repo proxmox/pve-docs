cfd83e21676db559b147093c3aeb8980f4d09b4a
Dietmar Maurer <dietmar@proxmox.com>
2016-03-23 08:49:38 +0100

    add missing file to generate datacenter.cfg options

---
+++ gen-datacenter-cfg-opts-adoc.pl
@@ -0,0 +1,10 @@
+#!/usr/bin/perl
+
+use strict;
+use warnings;
+use PVE::Cluster;
+use PVE::RESTHandler;
+
+my $schema = PVE::Cluster::get_datacenter_schema();
+
+print PVE::RESTHandler::dump_properties($schema->{properties});
