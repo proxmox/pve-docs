e7ff87618518ae851c9dff342c5f5059309c3b5a
Dietmar Maurer <dietmar@proxmox.com>
2016-11-05 17:21:04 +0100

    gen-pct-mountpoint-opts.pl: improve layout

--- gen-pct-mountpoint-opts.pl
+++ gen-pct-mountpoint-opts.pl
@@ -15,10 +15,14 @@ my $mp_prop = $prop->{mp0};
 
 my $typetext = PVE::JSONSchema::schema_get_type_text($rootfs_prop);
 
-print "`rootfs`: `$typetext`\n\n";
+print "`rootfs`: `$typetext` ::\n\n";
+
+print $rootfs_prop->{description} .
+    " See below for a detailed description of all options.\n\n";
 
 $typetext = PVE::JSONSchema::schema_get_type_text($mp_prop);
 
-print "`mp[n]`: `$typetext`\n\n";
+print "`mp[n]`: `$typetext` ::\n\n";
+print $mp_prop->{description} . "\n\n";
 
-print PVE::RESTHandler::dump_properties($mp_prop->{format});
+print PVE::RESTHandler::dump_properties($mp_prop->{format}, 'asciidoc', 'config-sub');
