8536358840cfcf77ffb27184bb322c210eb21e0d
Dietmar Maurer <dietmar@proxmox.com>
2016-11-20 09:27:40 +0100

    ha-manager.adoc: improve configuration section

---
+++ gen-ha-resources-opts.pl
@@ -0,0 +1,18 @@
+#!/usr/bin/perl
+
+use lib '.';
+use strict;
+use warnings;
+use PVE::RESTHandler;
+
+use Data::Dumper;
+
+use PVE::HA::Resources;
+
+
+my $private = PVE::HA::Resources::private();
+my $properies = $private->{propertyList};
+delete $properies->{type};
+delete $properies->{sid};
+
+print PVE::RESTHandler::dump_properties($properies);
