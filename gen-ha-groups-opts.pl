206c2476bf17a5d30fe27d5dc102fedf567b3e52
Dietmar Maurer <dietmar@proxmox.com>
2016-11-20 10:21:17 +0100

    ha-manager.adoc: improve group configuration section

---
+++ gen-ha-groups-opts.pl
@@ -0,0 +1,18 @@
+#!/usr/bin/perl
+
+use lib '.';
+use strict;
+use warnings;
+use PVE::RESTHandler;
+
+use Data::Dumper;
+
+use PVE::HA::Groups;
+
+
+my $private = PVE::HA::Groups::private();
+my $properies = $private->{propertyList};
+delete $properies->{type};
+delete $properies->{group};
+
+print PVE::RESTHandler::dump_properties($properies);
