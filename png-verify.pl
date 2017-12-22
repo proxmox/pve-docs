3bf6c8e65465a11c1a7a4743b4420318c299781f
Dietmar Maurer <dietmar@proxmox.com>
2017-12-22 11:36:31 +0100

    png-verify.pl: allow 72dpcm for pve installer images

--- png-verify.pl
+++ png-verify.pl
@@ -2,15 +2,27 @@
 
 use strict;
 use warnings;
+use File::Basename;
+
+my $installer_images = {
+    'pve-grub-menu.png' => 1,
+    'pve-installation.png' => 1,
+    'pve-select-location.png' => 1,
+    'pve-select-target-disk.png' => 1,
+    'pve-set-password.png' => 1,
+    'pve-setup-network.png' => 1,
+};
 
 my $infile = shift ||
     die "no input file specified\n";
 
-my $dpcm = 58; # expected
+my $basename = basename($infile);
+
+my $dpcm = $installer_images->{$basename} ? 72 : 58; # expected
 
 my $tmp = `identify -units PixelsPerCentimeter -format '%x x %y' $infile`;
 
-die "got unexpected density '$tmp' (fix with png-cleanup.pl)\n"
+die "$infile: got unexpected density '$tmp' (fix with png-cleanup.pl)\n"
     if $tmp ne "$dpcm x $dpcm";
 
 exit 0;
