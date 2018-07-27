508a8bd7f8b8925766b3a074241fde09c8fb35d8
Dietmar Maurer <dietmar@proxmox.com>
2018-07-27 09:54:44 +0200

    output-format.adoc: documentation for output format options

---
+++ gen-output-format-opts.pl
@@ -0,0 +1,13 @@
+#!/usr/bin/perl
+
+use lib '.';
+use strict;
+use warnings;
+use PVE::JSONSchema;
+use PVE::RESTHandler;
+
+my $prop = $PVE::RESTHandler::standard_output_options;
+
+my $data = PVE::RESTHandler::dump_properties($prop, 'asciidoc', 'config');
+
+print $data;
